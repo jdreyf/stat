ba19a8ca0ae65fadde69ddb672f2d0e279879800
jonathan dreyfuss <jdreyf@gmail.com>
2021-10-06 23:41:00 -0400

    Update crossval.R

--- crossval.R
+++ crossval.R
@@ -5,25 +5,21 @@ cv_soft <- function(y,
                    len = 20) {
   y <- as.matrix(y)
   Y2 <- y
+  # impute NAs with mean of scaled data
   Y2[is.na(Y2)] <- 0
   d <- dim(y)
   n <- d[1]
   p <- d[2]
   m <- sum(!is.na(y))
+  # svd$d gives singular values
   lambda1.max <- max(svd(Y2)$d)
   lambda1.min <- 1e-3*lambda1.max
-  grid.lambda1 <-
-    exp(seq(log(lambda1.min), log(lambda1.max), length.out = len))
-  ylist <-
-    lapply(1:N, function(k)
-      produce_NA(as.matrix(y),perc.missing = 0.2))$data.incomp
-  res.cv <- lapply(1:N, function(k) {
-    sapply(1:len,
-           function(i) {
-             yy <-produce_NA(as.matrix(y),perc.missing = 0.2)$data.incomp
-             res <-
-               softImpute(as.matrix(yy),
-                          lambda = grid.lambda1[i],maxit=1000)
+  grid.lambda1 <- exp(seq(log(lambda1.min), log(lambda1.max), length.out = len))
+  ylist <- lapply(1:N, function(k) produce_NA(as.matrix(y),perc.missing = 0.2))$data.incomp
+  res.cv <- lapply(1:N, function(k){
+    sapply(1:len, FUN=function(i){
+             yy <- produce_NA(as.matrix(y),perc.missing = 0.2)$data.incomp
+             res <- softImpute(as.matrix(yy), lambda = grid.lambda1[i],maxit=1000)
              u <- res$u
              d <- res$d
              v <- res$v
